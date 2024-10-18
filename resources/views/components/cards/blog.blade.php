454fe79b3e577f722004d35ea1fc92e2cadbad8d
QuocTuanIT <kutuanonline199@gmail.com>
2024-10-18 15:22:28 +0700

    add some code

--- resources/views/components/cards/blog.blade.php
+++ resources/views/components/cards/blog.blade.php
@@ -1,7 +1,7 @@
-<a href="#" class="group relative block h-64 sm:h-80 lg:h-80 bg-white dark:bg-gray-900 transition-transform duration-500">
+<a href="#" class="group relative block h-64 sm:h-80 lg:h-80 bg-white dark:bg-gray-900 transition-transform">
     <span class="absolute inset-0 border-2 border-dashed border-black rounded"></span>
 
-    <div class="relative flex h-full transform items-end border-2 rounded border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2"
+    <div class="relative flex h-full transform items-end border-2 rounded border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2 duration-400"
         style="background-image: url({{ asset('assets/images/hero-default.webp') }}); background-size: cover;">
 
         <div class="p-2 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-3 lg:p-4">
@@ -14,13 +14,14 @@
             class="w-full h-full" src="{{ asset('assets/images/hero-default.webp') }}"
             style="color: transparent;"> --}}
 
-            <h2 class="mt-4 text-xl font-medium sm:text-xl">Go around the world second around the world Go around the world second around the world</h2>
+            <h2 class="mt-4 text-xl font-medium sm:text-xl">Go around the world second around the world Go around the
+                world second around the world</h2>
             {{-- <p>time - author - view - like</p> --}}
         </div>
 
-        <div
-            class="absolute p-3 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100">
-            <h5 class="text-lg font-bold">Go around the world second around fsfdsfsfsfssdfsfsfsddfsdfsdfsfsfs the world Go around the world second around the world</h5>
+        <div class="absolute p-3 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100">
+            <h5 class="text-lg font-bold">Go around the world second around fsfdsfsfsfssdfsfsfsddfsdfsdfsfsfs the world
+                Go around the world second around the world</h5>
 
             <p class="mt-4 text-sm sm:text-sm">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, praesentium voluptatem
@@ -28,13 +29,10 @@
             </p>
 
             <div class="mt-4 font-bold flex items-center">
-                <div class="flex items-center border-2 border-gray-800 rounded p-2 bg-gray-900 text-white hover:bg-white hover:text-gray-800 transition-colors duration-150">
+                <div
+                    class="flex items-center border-2 border-gray-800 rounded p-2 bg-gray-900 text-white hover:bg-white hover:text-gray-800 transition-colors duration-300">
                     <span class="text-sm mr-1"> Read more </span>
-                    <svg class="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                    </svg>
+                    <x-icons.arrow-right />
                 </div>
             </div>
         </div>
